f3e81f2b51fc62280a3f5e11c207d9ba2f873e56
twdeveloper <twdeveloper@outlook.com>
2021-12-06 14:35:51 +0800

    v1 before

---
+++ ConsoleApp1/CurrencyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CurrencyConverter
+    {
+        public float Convert(string From, string To)
+        {
+            HttpClient hc = new HttpClient();
+            var ret = hc.GetAsync("https://free.currconv.com/api/v7/convert?q=USD_TWD&compact=ultra&apiKey=54bbaef1017ad8e12f43").Result;
+            var JSON = ret.Content.ReadAsStringAsync().Result;
+
+            dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(JSON);
+            return data.USD_TWD;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Financy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Financy
+    {
+        public double USD2TWD(double amount)
+        {
+            var currencyConverter = new CurrencyConverter();
+            double rate = currencyConverter.Convert("USD", "TWD");
+            return amount * rate;
+        }
+
+        public double SplitMoney(double USDAmount, int People)
+        {
+            var currencyConverter = new CurrencyConverter();
+            //使用到外部函式(抓取匯率)
+            double rate = currencyConverter.Convert("USD", "TWD");
+            //計算台幣總金額
+            double Total = USDAmount * rate;
+            //回傳一個人需要付多少錢(台幣)
+            return Total / People;
+        }
+    }
+}
